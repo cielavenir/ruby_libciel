c1c23a4414f712cb1d918e29c1da4835e23577c7
cielavenir <cielartisan@gmail.com>
2014-10-07 17:23:52 +0900

    Integarted coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,11 @@ RSpec.configure{|config|
 
 if !defined?(RUBY_ENGINE)||RUBY_ENGINE=='ruby'
 	require 'simplecov'
+	require 'coveralls'
+	SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+		SimpleCov::Formatter::HTMLFormatter,
+		Coveralls::SimpleCov::Formatter
+	]
 	SimpleCov.start do
 		add_filter 'spec'
 	end
